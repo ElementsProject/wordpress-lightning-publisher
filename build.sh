1a689db251803e2db9aa5d5d9420ed64242e8c68
Nadav Ivgi <nadav@shesek.info>
2017-12-09 21:27:38 +0200

    Exclude git directories from zip build, remove outdated exclusion

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 composer install --no-dev
-zip -r wordpress-lightning-paywall.zip composer.json lightning-paywall.php vendor css js --exclude='vendor/bacon/bacon-qr-code/tests/*'
+zip -r wordpress-lightning-paywall.zip composer.json lightning-paywall.php vendor css js --exclude='**/.git/*'
 
