d95be317991c12ca96cbb74bd27b8a326425ee72
Nadav Ivgi <nadav@shesek.info>
2017-12-10 16:53:37 +0200

    v0.1.2

--- lightning-paywall.php
+++ lightning-paywall.php
@@ -1,7 +1,7 @@
 <?php
 /*
     Plugin Name: Lightning Paywall
-    Version:     0.1.1
+    Version:     0.1.2
     Plugin URI:  https://github.com/ElementsProject/wordpress-lightning-paywall
     Description: Lightning paywall for WordPress posts
     Author:      Blockstream
