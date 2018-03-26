a5f4b219fa1c96bf19d9bfea865a4546ef9e5779
Nadav Ivgi <nadav@shesek.info>
2018-03-26 10:52:18 +0300

    Add README.md to distribution zip

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 composer install --no-dev
-zip -r wordpress-lightning-publisher.zip composer.json lightning-publisher.php vendor css js --exclude='**/.git/*'
+zip -r wordpress-lightning-publisher.zip composer.json lightning-publisher.php vendor css js README.md --exclude='**/.git/*'
 
